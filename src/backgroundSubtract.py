dcc6031d6f684a57f2ab5a45ecc3538dbf40cb3c
Zach Donsky <zachary.donsky@gmail.com>
2019-06-26 05:56:36 -0400

    streaming works in backgroundSubtract and face detect

--- src/backgroundSubtract.py
+++ src/backgroundSubtract.py
@@ -1,5 +1,9 @@
 import cv2 as cv
 import numpy as np
+import imutils
+from imutils.video import VideoStream
+import time
+
 
 ## foreground/background subtraction
 # fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
@@ -10,14 +14,23 @@ fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
 ## binary image black/white threshold
 threshold = 110
 
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream("rtsp://192.168.8.3:8554/unicast").start()
+time.sleep(2.0)
 def main():
-   cam = cv.VideoCapture(0)
+   # cam = cv.VideoCapture(0)
+
    while True:
-       ret, img = cam.read()
+       # ret, img = cam.read()
+       frame = vs.read()
+       frame = imutils.resize(frame, width=900)
 
        # convert cam feed to black and white
-       img_bw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-       img_binary = cv.threshold(img_bw, threshold, 255, cv.THRESH_BINARY)[1]
+       # not necessary for wyzecam, it's nightvision
+       # is already black and white
+       # img_bw = cv.cvtColor(blob, cv.COLOR_BGR2GRAY)
+       img_binary = cv.threshold(frame, threshold, 255, cv.THRESH_BINARY)[1]
        # apply foreground mask
        fgmask = fgbg.apply(img_binary)
        # not sure what this thing does
@@ -30,6 +43,6 @@ def main():
        # cv.imshow('webcam', img)
        if cv.waitKey(1) == 27:
            break # esc to quit
-   cam.release()
+   vs.release()
    cv.destroyAllWindows()
 main()
--- src/detect_faces_video.py
+++ src/detect_faces_video.py
@@ -4,18 +4,15 @@ import argparse
 import imutils
 import time
 import cv2
- 
-# construct the argument parse and parse the arguments
-
 
 # load our serialized model from disk
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe('deploy.prototxt.txt', 'res10_300x300_ssd_iter_140000.caffemodel')
- 
+net = cv2.dnn.readNetFromCaffe('neuralNets/deploy.prototxt.txt', 'neuralNets/res10_300x300_ssd_iter_140000.caffemodel')
+
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-#vs = VideoStream("rtsp://192.168.8.213:8554/unicast").start()
-vs = VideoStream().start()
+vs = VideoStream("rtsp://192.168.8.3:8554/unicast").start()
+# vs = VideoStream().start()
 
 time.sleep(2.0)
 
@@ -25,17 +22,17 @@ while True:
     # to have a maximum width of 400 pixels
     frame = vs.read()
     frame = imutils.resize(frame, width=900)
- 
+
     # grab the frame dimensions and convert it to a blob
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,(300, 300), (104.0, 177.0, 123.0))
- 
+
     # pass the blob through the network and obtain the detections and
     # predictions
     net.setInput(blob)
     detections = net.forward()
-    count = 0    
-    
+    count = 0
+
     # loop over the detections
     for i in range(0, detections.shape[2]):
         # extract the confidence (i.e., probability) associated with the
@@ -43,32 +40,32 @@ while True:
         confidence = detections[0, 0, i, 2]
         #print(confidence * 100)
 
- 
+
         # filter out weak detections by ensuring the `confidence` is
         # greater than the minimum confidence
         if confidence <.52:
             continue
-        count += 1 
+        count += 1
         # compute the (x, y)-coordinates of the bounding box for the
         # object
         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
         (startX, startY, endX, endY) = box.astype("int")
- 
+
         # draw the bounding box of the face along with the associated
         # probability
         text = "{:.2f}%".format(confidence * 100) + ", Count " + str(count)
         y = startY - 10 if startY - 10 > 10 else startY + 10
         cv2.rectangle(frame, (startX, startY), (endX, endY),(0, 255, 0), 2)
         cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-        
+
     # show the output frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
- 
+
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
- 
+
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
